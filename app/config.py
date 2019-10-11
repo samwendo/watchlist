2a5349389bc7b0aa615f5e9a8ea7ed00f7d2cfe1
samwendo <wendosam21@gmail.com>
2019-10-11 12:04:26 +0300

    add first commit

---
+++ app/config.py
@@ -0,0 +1,33 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+    MOVIE_API_KEY = '<6129bce4662969a52564c4517d4911de>'
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
